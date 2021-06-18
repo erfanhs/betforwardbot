c170ca0a7dc602a137bb91aa69f88db38addd03d
Erfan Harirsaz <erfanharirsaz071@gmail.com>
2021-06-18 15:16:49 +0430

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
-# login informations
-EMAIL = 'erfanharirsaz071@gmail.com'
-PASSWD = 'Erfan1381@'
+# login informations (betForward)
+EMAIL = ''
+PASSWD = ''
 # telegram bot
-TOKEN = '1559806560:AAF6iyl8NhEoeycJPSFx2MbM9quE7SYX9-M'
+TOKEN = ''
 ADMINS = [117053315, 68206644]
 # mongodb
 DBNAME = 'betForward'
